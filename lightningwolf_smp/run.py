fa7f5a720ec764964708388ad26a18f9a35db9f6
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2013-09-26 23:48:01 +0200

    different method to set configuration enviroment

---
+++ lightningwolf_smp/run.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding=utf8
+__author__ = 'ldath'
+
+from flask import (
+    redirect,
+    url_for,
+    flash,
+    render_template,
+    g
+)
+
+from lightningwolf_smp.application import app
+from lightningwolf_smp.blueprints import main
+from lightningwolf_smp.blueprints import login
+from lightningwolf_smp.blueprints import user
+
+
+"""Blueprints"""
+app.register_blueprint(main)
+app.register_blueprint(login)
+app.register_blueprint(user)
+
+
+@app.errorhandler(401)
+def authentication_failed(e):
+    flash('Authenticated failed.')
+    return redirect(url_for('login.login_page'))
+
+
+@app.errorhandler(403)
+def authorisation_failed(e):
+    flash(
+        (
+            'Your current identity is {id}. You need special privileges to access this page'
+        ).format(id=g.identity.user.username)
+    )
+
+    return redirect(url_for('login.login_page'))
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('jqueryuibootstrap_404.html', error=error), 404
+
+
+def main():
+    app.run()
--- lightningwolf_smp/smp.py
+++ lightningwolf_smp/smp.py
@@ -3,10 +3,10 @@
 """Lightningwolf Server Management Panel
 
 Usage:
-  smp.py start
+  smp.py [--config=<config>] start
   smp.py init:config
-  smp.py init:db
-  smp.py (user:create | user:password | user:delete) <username>
+  smp.py [--config=<config>] init:db
+  smp.py [--config=<config>] (user:create | user:password | user:delete) <username>
   smp.py (-h | --help)
   smp.py --version
 
@@ -16,54 +16,19 @@ Options:
   --credential=C  The C credential [default: admin] (admin|user)
 
 """
+import os
 from docopt import docopt
-
-from flask import (
-    redirect,
-    url_for,
-    flash,
-    render_template,
-    g
-)
-
-from lightningwolf_smp.application import app
-from lightningwolf_smp.blueprints import main
-from lightningwolf_smp.blueprints import login
-from lightningwolf_smp.blueprints import user
-
-
-"""Blueprints"""
-app.register_blueprint(main)
-app.register_blueprint(login)
-app.register_blueprint(user)
-
-
-@app.errorhandler(401)
-def authentication_failed(e):
-    flash('Authenticated failed.')
-    return redirect(url_for('login.login_page'))
-
-
-@app.errorhandler(403)
-def authorisation_failed(e):
-    flash(
-        (
-            'Your current identity is {id}. You need special privileges to access this page'
-        ).format(id=g.identity.user.username)
-    )
-
-    return redirect(url_for('login.login_page'))
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('jqueryuibootstrap_404.html', error=error), 404
+from lightningwolf_smp import version
 
 
 def main():
-    __version__ = app.config['VERSION']
-    arguments = docopt(__doc__, version=__version__)
+    arguments = docopt(__doc__, version=version)
+    if arguments['--config'] is not None:
+        os.environ["LIGHTNINGWOLF_SETTINGS"] = arguments['--config']
+
     if arguments['start']:
-        app.run()
+        from lightningwolf_smp.run import main
+        main()
     else:
         from lightningwolf_smp.utils.console import parse_arguments
         parse_arguments(arguments)
